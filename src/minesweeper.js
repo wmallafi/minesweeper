001e7ec48a410ca05b23809d7f68ded4528a5601
wmallafi <wmallafi@msn.com>
2017-10-09 20:40:48 -0400

    add printBoard formatting

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,14 +1,19 @@
+const printBoard = board => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
 
-const blankLine = '  |  |  ';
-const guessLine = '1 |  |  ';
-const bombLine = '  |B |  ';
+let board = [];
 
-console.log ('This is what an empty board would look like:');
-console.log (blankLine);
-console.log (blankLine);
-console.log (blankLine);
+board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
 
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log (blankLine);
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
